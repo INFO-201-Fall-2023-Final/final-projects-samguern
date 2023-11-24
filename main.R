e4885a28df95434589c2391aa846874e7b239c31
er-luo <er.caluo@gmail.com>
2023-11-24 01:38:18 -0800

    added age group column into mental health dataframe (for joining)

--- main.R
+++ main.R
@@ -1,3 +1,26 @@
-print("Test file")
 mentalHealth <- read.csv("mxmh_survey_results.csv")
 spotifyData <- read.csv("Spotify_data.csv")
+
+unique(spotifyData$Age)
+
+age_group <- function(age){
+  if(is.na(age) || age < 6){
+    return(NULL)
+  } 
+  
+  if(age > 60){
+    return("60+")
+  } else if (age >= 35){
+    return("35-60")
+  } else if (age >= 20){
+    return("20-35")
+  } else if (age >= 12){
+    return("12-20")
+  } else if (age >= 6){
+    return("6-12")
+  } 
+}
+
+mentalHealth$Age.group <-mapply(age_group, mentalHealth$Age)
+
+
