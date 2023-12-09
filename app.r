87c88cdbdc7891a6ef4ae20d7166402e08155e6e
samguern <147951613+samguern@users.noreply.github.com>
2023-12-09 15:13:08 -0800

    Update app.r

--- app.r
+++ app.r
@@ -2,35 +2,36 @@ library(shiny)
 
 ui <- fluidPage(
   
-  # App title ----
   titlePanel("My app"),
   
-  # Sidebar layout with input and output definitions ----
  # navbarPage("",
     
-    # Sidebar panel for inputs ----
     #sidebarPanel(),
     
-    # Main panel for displaying outputs ----
     #mainPanel(
       
-      # Output: Tabset w/ plot, summary, and table ----
     tabsetPanel(type = "tabs",
-                tabPanel("Intro", textOutput("intro")),
+                tabPanel("Intro", uiOutput("intro")),
                   # for boxed text tabPanel("Intro", verbatimTextOutput("intro")),
                 tabPanel("Story 1", plotOutput("story1")),
                 tabPanel("Story 2", textOutput("story2")),
-                  tabPanel("Story 3", tableOutput("story3"))
+                tabPanel("Story 3", tableOutput("story3"))
       )
       
   )
 
 
 server <- function(input, output){
-  
-  output$intro <- renderText({
-    "Intro"
+  output$intro <- renderUI({
+    tagList(
+      h2("Add image"),
+      p("This dataset is a summary of various statistics (music listening habits & personal mental health),",
+        "categorized by age group and filtered to Spotify users."),
+      #tags$img(src = "open-graph-default.png", width = 600)
+      #includeHTML((src = "~Desktop/open-graph-default.png"), width = 600)
+    )
   })
+  
   
 }
 
