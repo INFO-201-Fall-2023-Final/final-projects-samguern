fbf158e04797b0c7557ff66e5791c1ecbabdf18d
samguern <147951613+samguern@users.noreply.github.com>
2023-12-09 16:16:36 -0800

    Update main.R

--- main.R
+++ main.R
@@ -1,66 +1,59 @@
-library(shiny)
+library(dplyr)
+library(stringr)
+mentalHealth <- read.csv("mxmh_survey_results.csv")
+mentalHealth <- na.omit(mentalHealth)
+spotifyData <- read.csv("Spotify_data.csv")
+spotifyData <- na.omit(spotifyData)
 
-ui <- fluidPage(
-  
-  # App title ----
-  titlePanel("My app"),
-  
-  # Sidebar layout with input and output definitions ----
- # navbarPage("",
-    
-    # Sidebar panel for inputs ----
-    #sidebarPanel(),
-    
-    # Main panel for displaying outputs ----
-    #mainPanel(
-      
-      # Output: Tabset w/ plot, summary, and table ----
-    tabsetPanel(type = "tabs",
-                tabPanel("Intro", uiOutput("intro")),
-                  # for boxed text tabPanel("Intro", verbatimTextOutput("intro")),
-                tabPanel("Story 1", plotOutput("story1")),
-                tabPanel("Story 2", textOutput("story2")),
-                tabPanel("Story 3", tableOutput("story3"))
-      )
-      
-  )
+filtered_spofityData <- filter(spotifyData, str_detect(Age, "6-12") == FALSE)
+filtered_Spotify <- filter(mentalHealth, str_detect(Primary.streaming.service, "Spotify"),)
 
-
-server <- function(input, output){
-  output$intro <- renderUI({
-    tagList(
-      h2("Add image"),
-      p("This dataset is a summary of various statistics (music listening habits & personal mental health),",
-        "categorized by age group and filtered to Spotify users."),
-      ("Combined from two larger datasets (two different surveyed groups of people), this dataset was filtered down 
-       to easily determine various averaged characteristics of Spotify listeners, all grouped by age categories."),
-      ("Our summarized dataset finds:"),
-      br(),
-      ("Average Anxiety, Depression, and OCD"),
-      br(),
-      ("Average hours listened to music per day & what time of day"),
-      br(),
-      ("Listening device"),
-      br(),
-      ("Number of responses"),
-      br(),
-      ("Number that listen to music while working"),
-      br(),
-      h2("Why this is important:"),
-      p("Our data provides crucial information about mental health patterns and music listening habits, as categorized by age."),
-      ("It can be used in future implementation of medical treatments,"),
-      ("or developing apps aimed towards specific age categories"),
-      ("For example, according to our data, the age group 35-60 listens"),
-      ("to music the least amount of hours in a day. Therefore, this type"),
-      ("of user research shows how it would be least beneficial to market"),
-      ("a music app to this category. Or, it could potentially be beneficial"),
-      ("to know this information in aiming to target a new age category.")
-      #includeHTML((src = "~Desktop/open-graph-default.png"), width = 600)
-      #tags$img(src = "open-graph-default.png", width = 600),
-    )
-  })
+age_group <- function(age){
+  if(is.na(age) || age < 6){
+    return(NULL)
+  } 
   
-  
+  if(age > 60){
+    return("60+")
+  } else if (age >= 35){
+    return("35-60")
+  } else if (age >= 20){
+    return("20-35")
+  } else if (age >= 12){
+    return("12-20")
+  } else if (age >= 6){
+    return("6-12")
+  } 
 }
 
-shinyApp(ui = ui, server = server)
+filtered_Spotify$Age.group <-mapply(age_group, filtered_Spotify$Age)
+
+summarized_filtered_Spotify <- aggregate(cbind(Anxiety, Depression, OCD, Hours.per.day) ~ Age.group, data = filtered_Spotify, mean)
+
+getMode <- function(v){
+  uniquevar <- unique(v)
+  uniquevar[which.max(tabulate(match(v, uniquevar)))]
+}
+
+summarized_spotifyData <- aggregate(cbind(music_time_slot, spotify_listening_device) ~ Age, data = spotifyData, getMode)
+
+
+age_group_spotify_summary <- left_join(summarized_filtered_Spotify, summarized_spotifyData, by = c("Age.group" = "Age"))
+
+age_group_spotify_summary$Num_Responses <- 0
+
+age_counts_mentalh <- table(filtered_Spotify$Age.group) 
+age_counts_spotify <- table(filtered_spofityData$Age)
+
+for(i in 1:length(age_counts_mentalh)){
+  age_group_spotify_summary[i,"Num_Responses"] <- as.numeric(age_counts_mentalh[i]) + as.numeric(age_counts_spotify[i])
+}
+
+while_working_mentalh <- table(filter(filtered_Spotify, While.working == "Yes")$Age.group)
+while_working_spotify <- table(filter(filtered_spofityData , str_detect(music_lis_frequency, "Office|Study"))$Age)
+
+for(i in 1:length(while_working_mentalh)){
+  age_group_spotify_summary[i,"Listen_While_Working"] <- as.numeric(while_working_mentalh[i]) + as.numeric(while_working_spotify[i])
+}
+
+write.csv(age_group_spotify_summary, "C:/repo/final-projects-samguern/age_group_spotify_summary.csv", row.names=FALSE)
